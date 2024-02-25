5bdc8e737e99bbd4bd7d63abe00987bd269a53fc
cesarnavarro1011 <cesarnavarro1011+github@gmail.com>
2024-02-24 23:13:58 -0500

    photo

--- src/view/home.jsx
+++ src/view/home.jsx
@@ -1,11 +1,11 @@
 import { AspectRatio, Box, Image, NativeBaseProvider, Switch } from "native-base";
-import { logo } from '../../assets/logo_elecciones_2023.png';
+import { logo } from '../../assets/logo_electoral_2023.png';
 
 export default function Home() {
   return (
     <Box>
         <AspectRatio w="100%" ratio={12 / 9}>
-          <Image source={logo} alt="image" />
+          <Image source={require(logo)} alt="image" />
         </AspectRatio>
     </Box>
   );  
